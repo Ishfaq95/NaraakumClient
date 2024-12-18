893e80214141b510d74f8abbe4576ecd9fba7972
Muhammad Ishfaq <ishfaqyasin969@gmail.com>
2024-12-18 12:10:08 +0500

    changes

--- src/shared/redux/reducers/userReducer.ts
+++ src/shared/redux/reducers/userReducer.ts
@@ -14,7 +14,7 @@ const initialState: State = {
   topic: null,
   token: null,
   expiresAt: null,
-  appVersionCode: Platform.OS=="android"? "1.0.0":"1.0.0"
+  appVersionCode: Platform.OS=="android"? "1.0.0":"1.0.1"
 };
 
 export const userReducer = createSlice({
